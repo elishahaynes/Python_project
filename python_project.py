b5aa77c08d6c7e6437616d261afbf075bf82682a
Eli Haynes <elishahaynes@gmail.com>
2020-10-22 10:33:03 -0400

    Added rules for Rock, Paper, Scissors

---
+++ python_project.py
@@ -0,0 +1,8 @@
+import random 
+  
+# Print multiline instruction 
+# performstring concatenation of string 
+print("Winning Rules of the Rock paper scissor game as follows: \n"
+                                +"Rock vs paper->paper wins \n"
+                                + "Rock vs scissor->Rock wins \n"
+                                +"paper vs scissor->scissor wins \n")
