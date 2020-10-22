09a0f79368586fd09fce07e49926286983ec0f39
Eli Haynes <elishahaynes@gmail.com>
2020-10-22 11:58:42 -0400

    changed game and rules to 'I don't know what to eatgit add .'

--- python_project.py
+++ python_project.py
@@ -1,7 +1,14 @@
 import random 
   
 # Print instructions and rules on how to play game
-print("Winning Rules of 'Rock, Paper, Scissors' is: \n"
-    "Rock vs Paper ---> PAPER WINS! \n"
-    "Rock vs Scissors ---> ROCK WINS! \n"
-    "Paper vs Scissors ---> SCISSORS WINS! \n")
+# print("Winning Rules of 'Rock, Paper, Scissors' is: \n"
+#     "Rock vs Paper ---> PAPER WINS! \n"
+#     "Rock vs Scissors ---> ROCK WINS! \n"
+#     "Paper vs Scissors ---> SCISSORS WINS! \n")
+
+
+print("Welcome to 'I don't know what to eat!!' \n"
+    "1. View the list of ingredients by typing: ingredients \n"
+    "2. Choose your first ingredient. \n"
+    "3. Choose your second ingredient. \n"
+    "4. View the selected dish! \n")
