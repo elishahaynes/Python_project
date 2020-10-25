a12dec8eca0292e8eb3bc5aa47ccaa30f1f74fbc
Eli Haynes <elishahaynes@gmail.com>
2020-10-24 20:04:11 -0400

    added first and second ingredients code

--- python_project.py
+++ python_project.py
@@ -1,4 +1,4 @@
-import random 
+# import random 
   
 # Print instructions and rules on how to play game
 # print("Winning Rules of 'Rock, Paper, Scissors' is: \n"
@@ -7,8 +7,166 @@ import random
 #     "Paper vs Scissors ---> SCISSORS WINS! \n")
 
 
-print("Welcome to 'I don't know what to eat!!' \n"
-    "1. View the list of ingredients by typing: ingredients \n"
-    "2. Choose your first ingredient. \n"
-    "3. Choose your second ingredient. \n"
-    "4. View the selected dish! \n")
+
+
+
+
+
+
+
+
+
+
+
+# what I'm trying to create...
+# ingredients = [
+#     "potatoes",
+#     "broth",
+#     "tofu",
+# ]
+
+
+# def help():
+#     print("Welcome to your Meal Picker!")
+#     print("""
+# Enter 'Ingredients' to show the list of ingredients.
+# Enter 'DONE' to quit.
+# """)
+
+# help()
+# while True:
+#     item = input()
+
+#     if item == "DONE":
+#         break
+#     elif item == ingredients:
+#         print("Here are your Ingredients:")
+#         print(ingredients)
+    # else:
+    #     print("not working")
+
+
+
+
+
+# what I'm trying to create VERSION 2
+# Must run on python3
+ingredients = [
+    "potatoes",
+    "broth",
+    "tofu",
+]
+
+
+def help():
+    print("Welcome to your Meal Picker!")
+    print("""
+Enter 'Ingredients' to show the list of ingredients.
+Enter 'DONE' to quit.
+""")
+
+# def name_ingredient():
+#     input("Pick your first ingredient from the list! ")
+#     first_ingredient = pick_first_ingredient()
+
+
+
+def pick_second_ingredient():
+    second_ingredient = input("Pick your second ingredient from the list! ")
+
+
+help()
+while True:
+    item = input()
+
+    if item == "DONE":
+        break
+    elif item == "ingredients":
+        print("Here are your Ingredients:")
+        print(ingredients)
+        first_ingredient = input("Pick an ingredient! ")
+    
+        # pick_first_ingredient()
+        print("Your first ingredient is " + first_ingredient)
+        second_ingredient = input("Pick your last ingredient! ")
+        print("With your " + first_ingredient + " and " + second_ingredient + ", you should make...")
+        
+    # else:
+    #     print("not working")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#direct from Treehouse example
+# shopping_list = []
+
+
+# def show_help():
+#     print("What should we pick up at the store?")
+#     print("""
+# Enter 'DONE' to stop adding items.
+# Enter 'SHOW' to show list.
+# Enter 'HELP' for this help.
+# """)
+    
+    
+# def add_to_list(item):
+#     shopping_list.append(item)
+#     print("Your {} was added. You now have".format(item), len(shopping_list), "items in your list.")
+
+# def show_list():
+#     print("Here's your list:")
+#     for item in shopping_list:
+#         print("* " + item)
+    
+# show_help()
+# while True:
+#     new_item = input("> ")
+    
+#     if new_item == 'DONE':
+#         break
+#     elif new_item == 'HELP':
+#         show_help()
+#         continue
+#     elif new_item == 'SHOW':
+#         show_list()
+#         continue
+        
+#     add_to_list(new_item)
+    
+# show_list()
+
+
+
+
+
+
+# intro = input("Would you like to see the list of ingredients? Y/N ")
+# if intro.lower == "y":
+#     print(ingredients)
+# else:
+#     print("Have a great day!")
